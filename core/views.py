34309318f56fdbfa900e8c69de4e11cf5ac6a2ad
gonzalo99 <go.rojasf@duocuc.cl>
2021-12-29 21:27:52 -0300

    Paso a modulo Skills

--- core/views.py
+++ core/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
     return render(request, "core/index.html")
 def about(request):
     return render(request, "core/about.html")
+
+def skills(request):
+    return render(request, "core/skills.html")
--- portafolio/urls.py
+++ portafolio/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views as core_views
 urlpatterns = [
-    path('', core_views.index, name="index"),
-    path('/about', core_views.about, name="about"),
+    path('', core_views.home, name="home"),
+    path('about/', core_views.about, name="about"),
+    path('skills/', core_views.skills, name="skills"),
     path('admin/', admin.site.urls)
 ]
